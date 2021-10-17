f5910e5efbe5e3c5e2d1e30b374722e3864c1e0d
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-10-17 21:12:04 +0300

    npm install

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -9,7 +9,7 @@ function MoviesCard(props) {
 
   function isValidURL(string) {
     const res = string.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g
     );
     return res !== null;
   }

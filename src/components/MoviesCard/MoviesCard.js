9dff8e813f81d39c045cf0b21359683894ea39e9
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-10-29 21:49:29 +0300

    handleClickSave

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -43,19 +43,16 @@ function MoviesCard(props) {
   };
 
   const handleClickSave = () => {
-    const movieItem = props.savedMovies.filter(
-      (savedMovie) => savedMovie.movieId === movie.movieId
-    );
     if (!isSaved) {
       props.createMovie(movie);
       setIsSaved(true);
     } else {
+      const movieItem = props.savedMovies.filter(
+        (savedMovie) => savedMovie.movieId === movie.movieId
+      );
       props.deleteMovie(movieItem[0]._id);
       setIsSaved(false);
     }
-    props.setMovies((movies) =>
-      movies.map((m) => (m.nameRU === movie.nameRU ? movieItem.data : m))
-    );
   };
 
   const handleClickDelete = () => {

e4d37e7b79ceadd96d352722ecb41da745e9bfd0
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-09 22:38:51 +0300

    double then

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,22 +34,23 @@ function App(initialLoggedIn) {
 
   function updateMovies() {
     setIsLoading(true);
-    moviesApi
-      .getMovies()
-      .then((movies) => {
-        const newMovies = movies.forEach((item) => (item.saved = false));
-        savedMovies.length > 0
-          ? savedMovies.forEach((el) => {
-              const items = newMovies.map((i) =>
-                i.id === el.movieId ? Object.assign(i, { saved: true }) : i
-              );
-              localStorage.setItem("localMovies", JSON.stringify(items));
-              console.log(items);
-            })
-          : localStorage.setItem("localMovies", JSON.stringify(newMovies));
-      })
-      .catch((err) => console.log(`${err}`))
-      .finally(() => setIsLoading(false));
+    moviesApi.getMovies().then((res) =>
+      res
+        .forEach((item) => (item.saved = false))
+        .then((movies) =>
+          savedMovies.length > 0
+            ? savedMovies.forEach((el) => {
+                const items = movies.map((i) =>
+                  i.id === el.movieId ? Object.assign(i, { saved: true }) : i
+                );
+                localStorage.setItem("localMovies", JSON.stringify(items));
+                console.log(items);
+              })
+            : localStorage.setItem("localMovies", JSON.stringify(movies))
+        )
+        .catch((err) => console.log(`${err}`))
+        .finally(() => setIsLoading(false))
+    );
   }
 
   React.useEffect(() => {

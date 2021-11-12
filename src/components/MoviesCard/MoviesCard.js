69f1a68aca062097f1ae3c5d7ad2d6a71378cdf6
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-12 19:07:14 +0300

    saved:true

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -38,7 +38,7 @@ function MoviesCard(props) {
       : `https://api.nomoreparties.co${props.movie.image.formats.thumbnail.url}`,
     movieId: props.movie.id,
     _id: props.movie._id,
-    saved: props.movie.saved,
+    saved: true,
   };
 
   const movieDuration =

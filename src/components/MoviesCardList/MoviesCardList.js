9937438b7ceee7b2891a865598055773246af591
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-16 22:20:12 +0300

    search

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -69,7 +69,7 @@ function MoviesCardList(props) {
       }
       console.log("number");
     } else {
-      for (let i = 0; i < cardList.length; i++) {
+      for (let i = 0; i < numberOfMovies.length; i++) {
         cardList[i].style.display = "block";
       }
       console.log("card");

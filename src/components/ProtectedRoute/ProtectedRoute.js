665ec44576ac4482bcfb04547316296c623ed589
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-17 16:39:53 +0300

    two

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,6 @@ function ProtectedRoute({ component: Component, ...props }) {
   console.log(props.loggedIn);
   console.log({ component: Component, ...props });
   console.log(props);
-  console.log(...props);
   return (
     <Route>
       {() => (props.loggedIn ? <Component {...props} /> : <Redirect to="/" />)}

36dcf3cc6ec9bafb5ec540ddc59b1d3383613586
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-18 21:18:41 +0300

    back -3

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,19 +1,20 @@
+import React from "react";
 import { useHistory } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
   const history = useHistory();
 
-  function handleBack() {
-    history.go(-2);
-  }
+  const handleBack = () => {
+    history.go(-3);
+  };
 
   return (
     <section className="not-found">
       <div className="not-found__container">
         <h2 className="not-found__title">404</h2>
         <p className="not-found__subtitle">Страница не найдена</p>
-        <button type="button" onclick={handleBack} className="not-found__link">
+        <button onclick={handleBack} className="not-found__link">
           Назад
         </button>
       </div>

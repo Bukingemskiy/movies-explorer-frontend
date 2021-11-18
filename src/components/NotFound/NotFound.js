52184df576db4144358f438ca79f8c94dbe15494
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-18 21:29:48 +0300

    404 css

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,16 +4,16 @@ import "./NotFound.css";
 function NotFound() {
   const history = useHistory();
 
-  const handleBack = () => {
-    history.goBack();
-  };
-
   return (
     <section className="not-found">
       <div className="not-found__container">
         <h2 className="not-found__title">404</h2>
         <p className="not-found__subtitle">Страница не найдена</p>
-        <button onclick={handleBack} className="not-found__link">
+        <button
+          type="button"
+          onclick={() => history.go(-2)}
+          className="not-found__link"
+        >
           Назад
         </button>
       </div>

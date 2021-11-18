477b684102fcf7f213b1c78a8161da554b9d7c64
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-18 21:44:49 +0300

    add function back

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,16 +4,16 @@ import "./NotFound.css";
 function NotFound() {
   const history = useHistory();
 
+  const handleBack = () => {
+    history.go(-3);
+  };
+
   return (
     <section className="not-found">
       <div className="not-found__container">
         <h2 className="not-found__title">404</h2>
         <p className="not-found__subtitle">Страница не найдена</p>
-        <button
-          type="button"
-          onСlick={() => history.go(-2)}
-          className="not-found__link"
-        >
+        <button onClick={handleBack} className="not-found__link">
           Назад
         </button>
       </div>
